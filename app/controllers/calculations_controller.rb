0792aa61e86feda916a6eecd061893bc2672cf9b
Mohammad Ali Karimi <ali216k@gmail.com>
2023-03-10 21:13:12 +0000

    Display results for subtraction, multiplication and division

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -34,6 +34,10 @@ class CalculationsController < ApplicationController
 
     @result =  @second_number.to_f - @first_number.to_f
 
+    cookies.store(:subtraction_first, @first_number)
+    cookies.store(:subtraction_second, @second_number)
+    cookies.store(:subtraction_result, @result)
+
     render({ :template => "calculation_templates/subtract_results.html.erb" })
   end
 
@@ -43,6 +47,8 @@ class CalculationsController < ApplicationController
 
     @result =  @first_number.to_f * @second_number.to_f
 
+    cookies.store(:multiplication_result, @result)
+
     render({ :template => "calculation_templates/multiply_results.html.erb" })
   end
 
@@ -51,6 +57,8 @@ class CalculationsController < ApplicationController
     @second_number = params.fetch("second_num").to_f
 
     @result =  @first_number.to_f / @second_number.to_f
+
+    cookies.store(:division_result, @result)
 
     render({ :template => "calculation_templates/divide_results.html.erb" })
   end
